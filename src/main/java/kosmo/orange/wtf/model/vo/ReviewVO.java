be05b3220b1d68cf869b014430d0cdc252719b91
BBUBBABBABBU <73983783+BBUBBABBABBU@users.noreply.github.com>
2021-03-09 07:28:26 +0900

    광웅 210308
    리뷰작성화면 설계
    리뷰 피드 연결 작업중

--- src/main/java/kosmo/orange/wtf/model/vo/ReviewVO.java
+++ src/main/java/kosmo/orange/wtf/model/vo/ReviewVO.java
@@ -1,8 +1,26 @@
 package kosmo.orange.wtf.model.vo;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
 public class ReviewVO {
-    int taste;
-    int service;
-    int location;
-    int cleanness;
+
+    private String Review_id;
+    private String mem_id;
+    private String res_id;
+    private float avg;
+    private int taste;
+    private int service;
+    private int clean;
+    private int location;
+    private String content;
+    private String tag;
+    private String pic_loc;
+    private int rec_count;
+    private int charge_count;
+
 }
